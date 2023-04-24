ca3b799628c62f72909784171a7dfc1fcb38ec24
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-04-23 21:10:36 -0300

    arrumando o erro

--- src/queries/usuarioQuery.js
+++ src/queries/usuarioQuery.js
@@ -1,8 +1,8 @@
 const query = {
     SELECT_USUARIOS: 'SELECT * FROM usuarios ORDER BY created_at DESC LIMIT 50',
     SELECT_USUARIO: 'SELECT * FROM usuarios WHERE id = ?',
-    CREATE_USUARIO: 'INSERT INTO usuarios(nome, email, password) VALUES (?, ?, ?)',
-    UPDATE_USUARIO: 'UPDATE usuarios SET nome = ?, email = ? password = ? WHERE id = ?',
+    CREATE_USUARIO: 'INSERT INTO usuarios(name, email, senha) VALUES (?, ?, ?)',
+    UPDATE_USUARIO: 'UPDATE usuarios SET name = ?, email = ? senha = ? WHERE id_usuario = ?',
     DELETE_USUARIO: 'DELETE FROM usuarios WHERE id = ?'
 }
 
